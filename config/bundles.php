6b2a6c3f8c042763c666f8c40459422195fac4ca
stefanbusch-code <stefanbusch2011@gmail.com>
2025-04-15 09:13:22 +0200

    Filter auf der All Seite hinzugefügt als Dropdown und optisch angepasst

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
