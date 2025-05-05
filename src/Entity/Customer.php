920a78c28bea71b88504ae905de4bd1845155f4f
stefanbusch-code <stefanbusch2011@gmail.com>
2025-05-05 11:32:11 +0200

    alles alte zu Customer und Employee gelöscht
    neue customer und employee entitäten erstellt

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -3,80 +3,24 @@
 namespace App\Entity;
 
 use App\Repository\CustomerRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
-
 
 #[ORM\Entity(repositoryClass: CustomerRepository::class)]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_CUSTOMERNAME', fields: ['customername'])]
-class Customer implements UserInterface, PasswordAuthenticatedUserInterface
+class Customer
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\OneToMany(mappedBy: 'customer', targetEntity: Wishlist::class, cascade: ['persist','remove'])]
-    private Collection $wishlists;
-
-    public function __construct()
-    {
-        $this->wishlists = new ArrayCollection();
-    }
-
-    /**
-     * @return Collection<int, Wishlist>
-     */
-    public function getWishlists(): Collection
-    {
-        return $this->wishlists;
-    }
-
-    public function addWishlist(Wishlist $wishlist): static
-    {
-        if(!$this->wishlists->contains($wishlist))
-        {
-            $this->wishlists->add($wishlist);
-            $wishlist->setCustomer($this);
-        }
-        return $this;
-    }
-
-    public function deleteWishlist(Wishlist $wishlist): static
-    {
-        if($this->wishlists->removeElement($wishlist))
-        {
-            if($wishlist->getCustomer() === $this)
-            {
-                $wishlist->setCustomer(null);
-            }
-        }
-        return $this;
-    }
-
-    #[ORM\Column(length: 180)]
-    private ?string $customername = null;
-
-    /**
-     * @var list<string> The user roles
-     */
-    #[ORM\Column]
-    private array $roles = [];
-
-    /**
-     * @var string The hashed password
-     */
-    #[ORM\Column]
-    private ?string $password = null;
-
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $lastname = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $phone = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $street = null;
@@ -88,94 +32,19 @@ class Customer implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $zipcode = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $state = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $country = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $email = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $city = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $phone = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getCustomername(): ?string
-    {
-        return $this->customername;
-
-    }
-
-    public function setCustomername(string $customername): static
-    {
-        $this->customername = $customername;
-
-        return $this;
-    }
-
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUserIdentifier(): string
-    {
-        return (string) $this->customername;
-    }
-
-    /**
-     * @see UserInterface
-     *
-     * @return list<string>
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        $roles[] = 'ROLE_CUSTOMER';
-
-        return array_unique($roles);
-    }
-
-    /**
-     * @param list<string> $roles
-     */
-    public function setRoles(array $roles): static
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials(): void
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
     public function getFirstname(): ?string
     {
         return $this->firstname;
@@ -196,6 +65,18 @@ class Customer implements UserInterface, PasswordAuthenticatedUserInterface
     public function setLastname(?string $lastname): static
     {
         $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): static
+    {
+        $this->phone = $phone;
 
         return $this;
     }
@@ -248,30 +129,6 @@ class Customer implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function getCountry(): ?string
-    {
-        return $this->country;
-    }
-
-    public function setCountry(?string $country): static
-    {
-        $this->country = $country;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(?string $email): static
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
     public function getCity(): ?string
     {
         return $this->city;
@@ -284,14 +141,14 @@ class Customer implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function getPhone(): ?string
+    public function getCountry(): ?string
     {
-        return $this->phone;
+        return $this->country;
     }
 
-    public function setPhone(?string $phone): static
+    public function setCountry(?string $country): static
     {
-        $this->phone = $phone;
+        $this->country = $country;
 
         return $this;
     }
