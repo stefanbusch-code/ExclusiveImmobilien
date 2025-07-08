2344e9680fd67f0591acb856be4a180e108c207a
stefanbusch-code <stefanbusch2011@gmail.com>
2025-07-08 13:11:09 +0200

    Umstellung auf Flowbite Navbar neu

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,12 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', [
+        './assets/app.js',
+        './assets/styles/app.css'
+    ])
+
+    .enablePostCssLoader()
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
@@ -71,6 +76,8 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+
 ;
 
 module.exports = Encore.getWebpackConfig();
