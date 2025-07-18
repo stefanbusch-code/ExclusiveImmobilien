113244f7963c9ad4aa73619e351bbb5fd4f483ca
stefanbusch-code <stefanbusch2011@gmail.com>
2025-07-18 11:45:20 +0200

    pagination raus genommen

--- src/Controller/CreatePropertyController.php
+++ src/Controller/CreatePropertyController.php
@@ -93,77 +93,77 @@ final class CreatePropertyController extends AbstractController
     }
     #[Route('/createproperty/create', name: 'app_create_property.create')]
     public function createProperty (Request $request, EntityManagerInterface $entityManager): Response
+    {
+        //Formular
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+
+        $form->handleRequest($request);
+
+        //EntityManager
+        if ($form->isSubmitted() && $form->isValid())
         {
-            //Formular
-            $property = new Property();
-            $form = $this->createForm(PropertyType::class, $property);
+            $bild = $request->files->get('property')['bild'];
 
-            $form->handleRequest($request);
-
-            //EntityManager
-            if ($form->isSubmitted() && $form->isValid())
+            if($bild)
             {
-                $bild = $request->files->get('property')['bild'];
+                $dateiname =md5(uniqid(). '.'. $bild->guessClientExtension());
+            }
+            $bild->move
+            (
+                $this->getParameter('bilder_ordner'),
+                $dateiname
+            );
 
-                if($bild)
-                {
-                    $dateiname =md5(uniqid(). '.'. $bild->guessClientExtension());
-                }
-                $bild->move
-                (
-                    $this->getParameter('bilder_ordner'),
-                    $dateiname
-                );
+            $property = $form->getData();
+            $location = $property->getLocation();
 
-                $property = $form->getData();
-                $location = $property->getLocation();
+            if ($location !== null) {
 
-                if ($location !== null) {
-
-                    if (empty($location->getLocationTown())) {
-                        $this->addFlash('error', 'Town cannot be empty.');
-                        return $this->redirectToRoute('app_create_property.create');
-                    }
-
-                    if (!$location->getLocationZipcode()) {
-                        $location->setLocationZipcode('Bitte ein PLZ eingeben');
-                    }
-                } else {
-                    throw new \Exception("Location must be provided");
+                if (empty($location->getLocationTown())) {
+                    $this->addFlash('error', 'Town cannot be empty.');
+                    return $this->redirectToRoute('app_create_property.create');
                 }
 
-                $property->setBild($dateiname);
-
-                $entityManager->persist($property);
-                $entityManager->flush();
-
-                return $this->redirectToRoute('app_create_property.');
+                if (!$location->getLocationZipcode()) {
+                    $location->setLocationZipcode('Bitte ein PLZ eingeben');
+                }
+            } else {
+                throw new \Exception("Location must be provided");
             }
 
-            //Response
-            return $this->render('create_property/createProperty.html.twig', [
-                'createForm' => $form->createView(),
-            ]);
-        }
+            $property->setBild($dateiname);
 
-        #[Route('/createproperty/delete/{id}', name: 'app_create_property.delete')]
-        public function deleteProperty($id, PropertyRepository $pr, EntityManagerInterface $entityManager): Response
-        {
-            $property = $pr->find($id);
-            $entityManager->remove($property);
+            $entityManager->persist($property);
             $entityManager->flush();
-
-            $this->addFlash('erfolg','Immobile wurde erfolgreich gelöscht!');
 
             return $this->redirectToRoute('app_create_property.');
         }
 
-        #[Route('/createproperty/show/{id}', name: 'app_create_property.show')]
-            public function show(Property $property, EntityManagerInterface $entityManager): Response
-            {
-                return $this->render('create_property/show.html.twig', [
-                    'property' => $property,
-                ]);
-            }
+        //Response
+        return $this->render('create_property/createProperty.html.twig', [
+            'createForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/createproperty/delete/{id}', name: 'app_create_property.delete')]
+    public function deleteProperty($id, PropertyRepository $pr, EntityManagerInterface $entityManager): Response
+    {
+        $property = $pr->find($id);
+        $entityManager->remove($property);
+        $entityManager->flush();
+
+        $this->addFlash('erfolg','Immobile wurde erfolgreich gelöscht!');
+
+        return $this->redirectToRoute('app_create_property.');
+    }
+
+    #[Route('/createproperty/show/{id}', name: 'app_create_property.show')]
+    public function show(Property $property, EntityManagerInterface $entityManager): Response
+    {
+        return $this->render('create_property/show.html.twig', [
+            'property' => $property,
+        ]);
+    }
 
 }
